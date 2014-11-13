057344af0458317551df907b42d23f3c55fe4695
yangchangfei <yangcf@mapbar.com>
2014-11-13 17:58:15 +0800

    add ImagePicker file

---
+++ longfeiApp/longfeiApp/developercode/ImagePickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagePickerViewController.h
+//  longfeiApp
+//
+//  Created by yangcf on 14-11-13.
+//  Copyright (c) 2014年 yangcf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagePickerViewController : UIViewController
+
+@end
