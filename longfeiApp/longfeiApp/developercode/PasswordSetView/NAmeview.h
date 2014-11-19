cb29e993178a125be0e285af39612dcb4cdda816
feifei <yangcf@mapbar.com>
2014-11-19 22:13:35 +0800

    优化代码

--- longfeiApp/longfeiApp/developercode/PasswordSetView/NAmeview.h
+++ longfeiApp/longfeiApp/developercode/PasswordSetView/NAmeview.h
@@ -1,9 +1,9 @@
 //
 //  NAmeview.h
-//  ADDD
+//  feifei
 //
 //  Created by 206 on 13-7-9.
-//  Copyright (c) 2013年 吴丁虎. All rights reserved.
+//  Copyright (c) 2013年 feifei. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
